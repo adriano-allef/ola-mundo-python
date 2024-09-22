0064979216fe41def65c302517ec16801b9a1554
Adriano Allef Pereira <adrianopereira951@outlook.com>
2024-09-22 19:40:51 -0300

    Primeiro Commit

---
+++ ola-mundo.py
@@ -0,0 +1,15 @@
+print('Hello World!')
+
+nome = 'Adriano'
+      
+idade = 29
+
+peso = 72.9
+
+print(nome, idade, peso)
+
+idade = input('Qual é sua idade?')
+
+peso = input ('qual é o seu peso?')
+      
+print(nome, idade, peso)
